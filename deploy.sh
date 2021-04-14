5e06ecb28bb6d8cb4a57ca47a6d49ae9ecf602b1
David Toledo <david.b.toledo@outlook.com>
2021-04-14 11:51:59 +1000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:david-toledo/surfing-vue.git master:gh-pages
+git push -f https://github.com/david-Toledo/surfing-vue.git master:gh-pages
 cd -
